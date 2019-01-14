40b38274ac4e13a6d8056bbfbc1ed9ecdb07e661
Richard Gee <richard@technologee.co.uk>
2019-01-14 19:08:25 +0000

    Add tests
    Signed-off-by: Richard Gee <richard@technologee.co.uk>

--- function/handler.go
+++ function/handler.go
@@ -1,18 +1,31 @@
 package function
 
 import (
-	"fmt"
 	"strings"
 )
 
-// Handle a serverless request
-func Handle(req []byte) string {
+func reverse(domain string) string {
 
-	elems := strings.Split(string(req), ".")
+	if !strings.Contains(domain, ".") {
+		return domain
+	}
+
+	withoutScheme := strings.TrimPrefix(strings.TrimPrefix(domain, "http://"), "https://")
+	parts := strings.Split(withoutScheme, "/")
+	elems := strings.Split(parts[0], ".")
 
 	for i := len(elems)/2 - 1; i >= 0; i-- {
 		opp := len(elems) - 1 - i
 		elems[i], elems[opp] = elems[opp], elems[i]
 	}
-	return fmt.Sprintf("%s", strings.Join(elems, "."))
+
+	return strings.Join(elems, ".")
+
+}
+
+// Handle a serverless request
+func Handle(req []byte) string {
+
+	return reverse(string(req))
+
 }
